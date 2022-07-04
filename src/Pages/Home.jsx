074a8916a0f78dd041130794a094dabd3c212ab9
Xo'jayev Yo'ldoshbek <khojayev12@.com>
2022-07-04 14:33:42 +0500

    reaflow added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import AdminHome from "./AdminHome";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { Canvas } from "reaflow";
+import {IoExitOutline} from "react-icons/io5"
 
 export default function Home(props) {
   const [isAdmin, setIsAdmin] = useState(false);
@@ -17,6 +19,79 @@ const ClientHome = (props) => {
   const handleVisibility = () => {
     setShowSeriya(!showSeriya);
   };
+
+  const nodes = [
+    {
+      id: "1",
+      text: "1 gen",
+    },
+    {
+      id: "2",
+      text: "2 gen",
+    },
+    {
+      id: "3",
+      text: "3 gen",
+    },
+    {
+      id: "4",
+      text: "4 gen",
+    },
+    {
+      id: "5",
+      text: "5 gen",
+    },
+    {
+      id: "6",
+      text: "6 gen",
+    },
+    {
+      id: "7",
+      text: "7 gen",
+    },
+    {
+      id: "8",
+      text: "8 gen",
+    },
+  ];
+
+  const edges = [
+    {
+      id: "1-2",
+      from: "1",
+      to: "2",
+    },
+    {
+      id: "2-3",
+      from: "2",
+      to: "3",
+    },
+    {
+      id: "2-4",
+      from: "2",
+      to: "4",
+    },
+    {
+      id: "3-5",
+      from: "3",
+      to: "5",
+    },
+    {
+      id: "3-6",
+      from: "3",
+      to: "6",
+    },
+    {
+      id: "4-7",
+      from: "4",
+      to: "7",
+    },
+    {
+      id: "4-8",
+      from: "4",
+      to: "8",
+    },
+  ];
 
   return (
     <div className="client-home">
@@ -45,22 +120,39 @@ const ClientHome = (props) => {
         <div className="client-info-text client-info-right">
           <div className="pasport-info-right">12345</div>
         </div>
-        <div className="tolashi-kerak"><strong>To'lash kerak bo'lgan summa:</strong></div>
-        <div className="client-info-text client-info-left">Bir narsa uchun:</div>
+        <div className="tolashi-kerak">
+          <strong>To'lash kerak bo'lgan summa:</strong>
+        </div>
+        <div className="client-info-text client-info-left">
+          Bir narsa uchun:
+        </div>
         <div className="client-info-text client-info-right">1,000,000 so'm</div>
-        <div className="client-info-text client-info-left">Bir narsa uchun:</div>
+        <div className="client-info-text client-info-left">
+          Bir narsa uchun:
+        </div>
         <div className="client-info-text client-info-right">1,000,000 so'm</div>
-        <div className="client-info-text client-info-left">Bir narsa uchun:</div>
+        <div className="client-info-text client-info-left">
+          Bir narsa uchun:
+        </div>
         <div className="client-info-text client-info-right">1,000,000 so'm</div>
-        <div className="client-info-text client-info-left">Jami qo'shilgan odamlar:</div>
+        <div className="client-info-text client-info-left">
+          Jami qo'shilgan odamlar:
+        </div>
         <div className="client-info-text client-info-right">3</div>
-        <div className="client-info-text client-info-left">Qancha odam qo'shish kerak:</div>
+        <div className="client-info-text client-info-left">
+          Qancha odam qo'shish kerak:
+        </div>
         <div className="client-info-text client-info-right">2</div>
         <div className="client-info-text client-info-left">Minimum bonus:</div>
         <div className="client-info-text client-info-right">10%</div>
-        <div className="client-info-text client-info-left">Bonusdan keyingi summa:</div>
+        <div className="client-info-text client-info-left">
+          Bonusdan keyingi summa:
+        </div>
         <div className="client-info-text client-info-right">2,700,000 so'm</div>
       </div>
+
+      <Canvas className="diag" nodes={nodes} edges={edges} width="50%" height="70vh" maxHeight={1000} maxWidth={1000} />
+      <div className="client-log-out" >Tizimdan chiqish <IoExitOutline style={{fontSize:"29px", position:"fixed", top:"6.5px"}} /> </div>
     </div>
   );
 };
